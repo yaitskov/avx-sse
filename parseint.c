fcbc4035bddb5d3f10d01e1bc7daf704bc4eec58
Daniil Iaitskov <dyaitskov@gmail.com>
2024-02-20 22:36:05 -0500

    master parseint 4 digit number

--- parseint.c
+++ parseint.c
@@ -21,7 +21,7 @@ int main() {
   // __m256i result = _mm256_set_epi64x(0, 0, 0, 0);
   //  __m256i digitKs = _mm256_set_epi32(10_000_000, 1000_000, 100_000, 10_000, 1000, 100, 10, 1);
   __m256i digitBase10 = _mm256_set_epi32(0, 0, 0, 0, '0', '0', '0', '0');
-  __m256i digitKs = _mm256_set_epi32(1, 1, 1, 0, 1, 10, 100, 1000);
+  __m256i digitKs = _mm256_set_epi32(0, 1, 0, 100, 0, 10, 0, 1000);
   // __m256i i = _mm256_load_si256((__m256i*) input);
   __m128i i = _mm_load_si128((__m128i*) input);
   __m256i cvt8_32 = _mm256_cvtepi8_epi32(i);
@@ -31,15 +31,25 @@ int main() {
   m256_print_epi32("base-10", &digitBase10);
   __m256i sub = _mm256_sub_epi32(cvt8_32, digitBase10);
   m256_print_epi32("sub    ", &sub);
-
+  // shuffle_epi32 cannot move data between 128 buckets
+  // let's try _mm256_extracti128_si256 and _mm256_inserti128_si256
+  __m256i sub2 = _mm256_inserti128_si256(
+                                         sub,
+                                         _mm_srli_si128(
+                                                        _mm256_extracti128_si256(sub, 0),
+                                                        4
+                                                        ),
+                                         1);
+  m256_print_epi32("sub2   ", &sub2);
+                          // _mm256_shuffle_epi32(sub, 0b_03_02_01_00_03_02_01_00);
   m256_print_epi32("digitKs", &digitKs);
-  __m256i p = _mm256_mul_epi32(sub, digitKs);
+  __m256i p = _mm256_mul_epi32(sub2, digitKs);
 
   long r0 = _mm256_extract_epi64(p, 0);
   long r1 = _mm256_extract_epi64(p, 1);
   long r2 = _mm256_extract_epi64(p, 2);
   long r3 = _mm256_extract_epi64(p, 3);
   long result = r0 + r1 + r2 + r3;
-  printf("%d; %s = %d = %d + %d + %d + %d\n", sizeof(long), input, (int) result, r0, r1, r2, r3);
+  printf("%ld; %s = %ld = %ld + %ld + %ld + %ld\n", sizeof(long), input, result, r0, r1, r2, r3);
   return 0;
 }
